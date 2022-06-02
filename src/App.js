84e558187c212dc41030dff50173e1738f6c2745
Sachin Priyadarshi <97968286+insach67@users.noreply.github.com>
2022-06-03 00:35:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import UnMount from './Component/UnMount';
+// import DidUpdate from './Component/DidUpdate';
+// import GetSnap from './Component/GetSnap';
+// import Update from './Component/Update';
+// import GetDerived2 from './Component/GetDerived2';
+// import GetDerive from './Component/GetDerive';
+function App() {
+  return (
+    <div className="App">
+
+{/* <Constructor /> */}
+{/* <GetDerive favColour="yellow" /> */}
+
+{/* <GetDerived2 favColour="yellow" /> */}
+{/* <Update /> */}
+
+{/* <GetSnap /> */}
+
+{/* <DidUpdate /> */}
+
+<UnMount />
+
+    </div>
+  );
+}
+
+export default App;
